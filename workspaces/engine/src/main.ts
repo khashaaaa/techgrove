bb94625f29f85199f6aca76135c82a317740afeb
khashaaaa <forwardent01@gmail.com>
2023-11-11 21:42:59 +0800

    Cors

--- workspaces/engine/src/main.ts
+++ workspaces/engine/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
+    origin: 'http://140.99.243.243',
+    // origin: '*',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS'
   })
   const port = process.env.PROG_PORT || 8080
